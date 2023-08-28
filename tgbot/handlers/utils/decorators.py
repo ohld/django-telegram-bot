f151d781a8ec5c9e4bc6be1c748b33116ded615a
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-28 20:09:13 +0330

    Move 'send_typing_action' to decorators.py

--- tgbot/handlers/utils/decorators.py
+++ tgbot/handlers/utils/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from typing import Callable
 
-from telegram import Update
+from telegram import Update, ChatAction
 from telegram.ext import CallbackContext
 
 from users.models import User
@@ -30,3 +30,14 @@ def admin_only(reply_message: str = None):
         return inner
 
     return decorator
+
+
+def send_typing_action(func: Callable):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update: Update, context: CallbackContext, *args, **kwargs):
+        update.effective_chat.send_chat_action(ChatAction.TYPING)
+        return func(update, context, *args, **kwargs)
+
+    return command_func
