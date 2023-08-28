a472198c6b855efa719f577e9095668125bae6ff
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-28 13:54:19 +0330

    Add 'admin_only' decorator

--- tgbot/handlers/admin/handlers.py
+++ tgbot/handlers/admin/handlers.py
@@ -6,26 +6,20 @@ from telegram.ext import CallbackContext
 
 from tgbot.handlers.admin import static_text
 from tgbot.handlers.admin.utils import _get_csv_from_qs_values
+from tgbot.handlers.utils.decorators import admin_only
 from tgbot.handlers.utils.info import send_typing_action
 from users.models import User
 
 
+@admin_only()
 def admin(update: Update, context: CallbackContext) -> None:
     """ Show help info about all secret admins commands """
-    u = User.get_user(update, context)
-    if not u.is_admin:
-        update.message.reply_text(static_text.only_for_admins)
-        return
     update.message.reply_text(static_text.secret_admin_commands)
 
 
+@admin_only()
 def stats(update: Update, context: CallbackContext) -> None:
     """ Show help info about all secret admins commands """
-    u = User.get_user(update, context)
-    if not u.is_admin:
-        update.message.reply_text(static_text.only_for_admins)
-        return
-
     text = static_text.users_amount_stat.format(
         user_count=User.objects.count(),  # count may be ineffective if there are a lot of users.
         active_24=User.objects.filter(updated_at__gte=now() - timedelta(hours=24)).count()
@@ -38,14 +32,10 @@ def stats(update: Update, context: CallbackContext) -> None:
     )
 
 
+@admin_only()
 @send_typing_action
 def export_users(update: Update, context: CallbackContext) -> None:
-    u = User.get_user(update, context)
-    if not u.is_admin:
-        update.message.reply_text(static_text.only_for_admins)
-        return
-
     # in values argument you can specify which fields should be returned in output csv
     users = User.objects.all().values()
     csv_users = _get_csv_from_qs_values(users)
-    context.bot.send_document(chat_id=u.user_id, document=csv_users)
+    update.message.reply_document(csv_users)
---
+++ tgbot/handlers/utils/decorators.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from typing import Callable
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from tgbot.handlers.admin import static_text
+from users.models import User
+
+
+def admin_only(reply_message: str = static_text.only_for_admins):
+    """
+    Admin only decorator
+    Used for handlers that only admins have access to
+
+    @param reply_message: message to reply
+    """
+
+    def decorator(func: Callable):
+        @wraps(func)
+        def inner(update: Update, context: CallbackContext, *args, **kwargs):
+            user = User.get_user(update, context)
+
+            if not user.is_admin:
+                if reply_message is not None and update.effective_message:
+                    update.effective_message.reply_text(reply_message)
+                return
+
+            return func(update, context, *args, **kwargs)
+
+        return inner
+
+    return decorator
